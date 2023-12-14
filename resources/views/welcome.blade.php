7303161c9893092d7544befbdcdb22215037e1f7
Courtney Elsner <courtneyelsner@pop-os.localdomain>
2023-12-14 11:59:21 -0400

    Add Docker using Laravel Sail.

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,8 +6,12 @@
     <ul>
         @foreach ($tasks as $task)
             <li>
-                <h2>
-                <a href="/tasks/{{ $task->id }}">{{ $task->title }}</a>
+		<h2>
+			@if($tasks->isEmpty())
+    				<p>No tasks found.</p>
+			@else
+				<a href="/tasks/{{ $task->id }}">{{ $task->title }}</a>
+			@endif
                 </h2>
             </li>
         @endforeach
